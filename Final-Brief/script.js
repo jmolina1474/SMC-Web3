7c7fce830944cb6d833cefd55f7a0d56761b80fb
Jason Molina <70608137+jmolina1474@users.noreply.github.com>
2021-05-31 21:17:50 -0700

    Final Brief
    Added Multiple Pages for final brief.
    Added Navbar to pages.
    Added animations to elements.
    Added filler text and some images to pages.
    Added Carousel to products page.
    Added CSS and Javascript to pages.
    Added item product page.
    Added checkout page and contact page with validation.

---
+++ Final-Brief/script.js
@@ -0,0 +1,74 @@
+(function () {
+    'use strict'
+
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+// Cart Scripts
+$(document).ready(function(){
+  
+  $("#cart-1").click(function(){
+    $("#empty, #item1, #checkout").toggle();
+ })
+
+ $("#remove").click(function(){
+  $("#empty, #item1, #checkout").toggle();
+})
+
+});
+
+// carousel --------------------------------------------------------------
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
